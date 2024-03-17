8d66718616ec9424fe2248e33adb0a7cbaa8a5ab
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-17 17:21:38 +0100

    Add array task in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
+const tasks =[];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
