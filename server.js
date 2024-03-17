58e05bd2524af7f88e28125e2d55899bd106262b
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-17 16:53:46 +0100

    Add middleware for case if path is not found in server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res) => {
+    const failure = { message: 'Not found...' };
+  
+    res.status(404).json(failure);
+  });
+  
 const server = app.listen(8000, () => {
     console.log('Server is running on port: 8000');
   });
