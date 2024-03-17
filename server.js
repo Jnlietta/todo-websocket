1c7b901f994539ae5d04dac7d286ee720d8d258c
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-17 17:56:42 +0100

    Add listeners addTask and removeTask with same emiters inside to other users in server.js

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ const io = socket(server);
 io.on('connection', (socket) => {
     //emiter to socket that is serving now (new user)
     socket.emit('ubdateData', tasks)
+
+    //listener for event addTask
+    socket.on('addTask', (task) => {
+        //add task to array tasks, task = { name, id }
+        tasks.push(task);
+
+        //emiter for other users that there is new task
+        socket.broadcast.emit('addTask', task);
+    });
+
+    //listener for event removeTask
+    socket.on('removeTask', (taskID) => {
+        //find task that have to be delete from array tasks and remove it
+        const task = tasks.find(task => task.id === taskID);
+        const indexOfTask = tasks.indexOf(task);
+        users.splice(indexOfTask, 1);
+
+        //emiter for other users that there is deleted task
+        socket.broadcast.emit('removeTask', taskID);
+    });
   });
 
 app.use((req, res) => {
